234c33ace6a50161642aa442644df84dd894e1ff
anusha <hanusha9391@gmail.com>
2024-11-01 21:08:08 +0530

    this is first commit

---
+++ program1/src/App.jsx
@@ -0,0 +1,10 @@
+import Program from "./program";
+
+function App(){
+  return(
+    <>
+    <Program />
+    </>
+  )
+}
+export default App;
---
+++ program1/src/Program.jsx
@@ -0,0 +1,8 @@
+function Program(){
+    return(
+        <>
+        <h1>Program</h1>
+        </>
+    )
+}
+export default Program;
